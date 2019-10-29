20daef602369b49d2005c84f25c8c1d9c421fb44
Mvflyin <2996578238@qq.com>
2019-10-29 17:21:12 +0800

    blah

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'slug', 'text', 'short_description', 'category', 'tags')
+        fields = ('title', 'slug', 'text', 'short_description', 'category', 'tags', 'related_img')
 
 class CommentForm(forms.ModelForm):
 
